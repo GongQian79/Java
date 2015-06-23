91af6c0ebcb93d81d84de0c2f82a0cbdea0dc045
GongQian79 <gongqian.de@googlemail.com>
2015-06-24 07:04:47 +0800

    nichts neu

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		method();
+	}
+
+	private static void method() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i=" + i);
+		}
+	}
+	
+}
